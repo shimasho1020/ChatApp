6369ad82635a32fbba92340c46f0e821dcb8726e
shimasho1020 <shimasho1020@gmail.com>
2022-12-31 15:10:27 +0900

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 島田将太郎 on 2022/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
