4e55994ad05dd6fc99531dbd106eed229d185676
shimasho1020 <shimasho1020@gmail.com>
2023-01-01 16:35:46 +0900

    connect firebase

---
+++ ChatApp/CreatAccountView.swift
@@ -0,0 +1,78 @@
+//
+//  CreatAccountView.swift
+//  ChatApp
+//
+//  Created by 島田将太郎 on 2022/12/31.
+//
+
+import UIKit
+
+class CreatAccountView: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onImage)))
+    }
+    
+    @objc func onImage(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = ["public.image"]
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
+    
+    @IBAction func onResister(_ sender: Any) {
+        if nameField.text!.count < 3 || nameField.text!.count > 11 {
+            showError(message: "名前は3字以上10字以内で設定してください。")
+            return
+        }
+        AuthHelper().createAccount(email: emailField.text!, password: passwordField.text!, result: {
+            success in
+            if success {
+//                DatabaseHelper(uid: AuthHelper().uid()).resisterUserInfo(name: self.nameField.text!, image: self.imageView.image!)
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                self.showError(message: "有効なメールアドレス、6文字以上のパスワードを設定してください。")
+            }
+        })
+    }
+    @IBAction func onBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func showError(message:String){
+        let dialog = UIAlertController(title: "エラー", message: message, preferredStyle: .alert)
+        dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(dialog, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ChatApp/DatabaseHelper.swift
@@ -0,0 +1,87 @@
+//
+//  DatabaseHelper.swift
+//  ChatApp
+//
+//  Created by 島田将太郎 on 2023/01/01.
+//
+
+import Foundation
+import Firebase
+//import SDWebImage
+import FirebaseFirestore
+import FirebaseStorage
+//import FirebaseStorageUI
+
+class DatabaseHelper {
+    
+    let uid = AuthHelper().uid()
+    let db = Firestore.firestore()
+    let storage = Storage.storage().reference()
+    
+    func getMyRoomList(result:@escaping([ChatRoom]) -> Void){
+        var roomList:[ChatRoom] = []
+        db.collection("room").whereField("user", arrayContains: uid).addSnapshotListener({
+            (querySnapshot, error) in
+            if error == nil {
+                for doc in querySnapshot!.documents {
+                    let data = doc.data()
+                    guard let users = data["user"] as? [String] else { return }
+                    if users.count != 2 { return }
+                    var user = ""
+                    if users[0] == self.uid {
+                        user = users[1]
+                    } else {
+                        user = users[0]
+                    }
+                    roomList.append(ChatRoom(roomID:doc.documentID, userID: user))
+                }
+                result(roomList)
+            }
+        })
+    }
+    
+    func getUserInfo(userID:String,result:@escaping(String) -> Void){
+        db.collection("user").document(userID).getDocument(completion: {
+            (querySnapshot, error) in
+            if error == nil {
+                let data = querySnapshot?.data()
+                guard let name = data!["name"] as! String? else {
+                    result("")
+                    return
+                }
+                result(name)
+            } else {
+                result("")
+            }
+        })
+    }
+    
+    func getUserName(userID:String,result:@escaping(String) -> Void) {
+        db.collection("user").document(userID).getDocument(completion: {
+            (doc, error) in
+            if error == nil {
+                let data = doc?.data()
+                guard let name = data!["name"] as! String? else { return }
+                result(name)
+            }
+        })
+    }
+
+//    func resisterUserInfo(name:String,image:UIImage){
+//        db.collection("user").document(uid).setData(["name":name])
+//        let resized = image.resize(toWidth: 300)
+//        guard let imageData = resized!.jpegData(compressionQuality:1) else { return }
+//        storage.child("image/\(uid).jpeg").putData(imageData, metadata: nil)
+//    }
+//
+//    func getImage(userID:String,imageView:UIImageView){
+//        let imageRef = storage.child("image/"+userID+".jpeg")
+////        imageView.sd_setImage(with: imageRef)
+//    }
+    
+}
+
+struct ChatRoom {
+    let roomID:String
+    let userID:String
+}
